d11c5a54c48c5df985fd10733bfe609faff5581a
Vivek Hiwarkar <vivek.hiwarkar@gmail.com>
2018-07-16 11:26:12 +0530

    Final Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {coerceNumberProperty} from '@angular/cdk/coercion';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor (private httpService: HttpClient) { }
+  arrBirds: string [];
+
+  ngOnInit () {
+    this.httpService.get('./assets/birds.json').subscribe(
+      data => {
+        this.arrBirds = data as string [];	 // FILL THE ARRAY WITH DATA.
+        //  console.log(this.arrBirds[1]);
+      },
+      (err: HttpErrorResponse) => {
+        console.log (err.message);
+      }
+    );
+  }
+  
+  autoTicks = false;
+  max = 10;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  
+
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : 0;
+  }
+  set tickInterval(value) {
+    this._tickInterval = coerceNumberProperty(value);
+  }
+  private _tickInterval = 1;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    HttpClientModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+
 })
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule); 
